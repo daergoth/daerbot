2f0c1bac597b668d4dd197c212a202496834e2bc
István Lakatos <atyep95@msn.com>
2017-10-07 16:48:35 +0200

    Logging now can be set server-wide, not bot-wide

--- src/command_modules/log.js
+++ src/command_modules/log.js
@@ -1,38 +1,5 @@
-const ContentRegExpHandler = require("../content-regexp-handler.js");
 const configuration = require("../configuration");
-
-var logChannelName = configuration.getConfig("log.channelName", "log");
-var logStatus = configuration.getConfig("log.status", false);
-
-var logListener = (oldMember, newMember) => {
-    let logChannel = oldMember.guild.channels.find(c => c.name === logChannelName);
-
-    if (!logChannel) {
-        oldMember.guild.createChannel(logChannelName, "text")
-            .then(textChannel => {
-                console.log("Created channel: ", textChannel.name);  
-                logChannel = textChannel;
-
-                handleStatusUpdate();
-            })
-            .catch(error => console.log(error));
-    } else {
-        handleStatusUpdate();
-    }
-
-    function handleStatusUpdate() {
-        if (!oldMember.voiceChannel) {
-            logChannel.send(`${oldMember.user} connected to ${newMember.voiceChannel}!`);
-        } else if (!newMember.voiceChannel) {
-            logChannel.send(`${oldMember.user} disconnected from ${oldMember.voiceChannel}!`);
-        } else {
-            if (oldMember.voiceChannel != newMember.voiceChannel) {
-                logChannel.send(`${oldMember.user} switched from ${oldMember.voiceChannel} to ${newMember.voiceChannel}`);
-            }
-        }
-    }
-
-};
+const ContentRegExpHandler = require("../content-regexp-handler.js");
 
 const LogToggleHandler = {
     LogToggleHandler() {
@@ -40,9 +7,48 @@ const LogToggleHandler = {
 
         this.ContentRegExpHandler(/^\.logtoggle/);
     },
-    handle(message) {
+    handle(message, storage) {
+        let logStatus = storage.getFromGuildLevel(message.guild, "log.status", true,
+            configuration.getConfig("log.status", true));
+
         logStatus = !logStatus;
-        configuration.setConfig("log.status", logStatus);
+
+        storage.saveOnGuildLevel(message.guild, "log", {
+            status: logStatus
+        });
+
+        let logListener = (oldMember, newMember) => {
+            let logChannelName = storage.getFromGuildLevel(message.guild, "log.channelName", true, 
+                configuration.getConfig("log.channelName", "log"));
+        
+            let logChannel = oldMember.guild.channels.find(c => c.name === logChannelName);
+        
+            if (!logChannel) {
+                oldMember.guild.createChannel(logChannelName, "text")
+                    .then(textChannel => {
+                        console.log("Created channel: ", textChannel.name);
+                        logChannel = textChannel;
+        
+                        handleStatusUpdate();
+                    })
+                    .catch(error => console.log("Error while creating log channel: " + error));
+            } else {
+                handleStatusUpdate();
+            }
+        
+            function handleStatusUpdate() {
+                if (!oldMember.voiceChannel) {
+                    logChannel.send(`${oldMember.user} connected to ${newMember.voiceChannel}!`);
+                } else if (!newMember.voiceChannel) {
+                    logChannel.send(`${oldMember.user} disconnected from ${oldMember.voiceChannel}!`);
+                } else {
+                    if (oldMember.voiceChannel != newMember.voiceChannel) {
+                        logChannel.send(`${oldMember.user} switched from ${oldMember.voiceChannel} to ${newMember.voiceChannel}`);
+                    }
+                }
+            }
+        
+        };
 
         if (logStatus) {
             message.guild.client.on("voiceStateUpdate", logListener);
@@ -62,7 +68,10 @@ const LogStatusHandler = {
 
         this.ContentRegExpHandler(/^\.logstatus/);
     },
-    handle(message) {
+    handle(message, storage) {
+        let logStatus = storage.getFromGuildLevel(message.guild, "log.status", true,
+            configuration.getConfig("log.status", true));
+
         message.channel.send(`Logging status: ${logStatus}`);
     }
 };
